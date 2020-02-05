77244ecf4d3d28b6617dd8e462cb7f40968b8598
charliewindred <charliewindred@gmail.com>
2020-02-05 11:01:20 +0000

    Created Order test and Order class, deleted various problematic user specific settings paths from csproj files.

---
+++ Testing/Testing/tstOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InstanceOK()
+        { 
+            clsOrder order = new clsOrder();
+            Assert.IsNotNull(order);
+        
+
+        
+        }
+    }
+}
